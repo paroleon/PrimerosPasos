be888e6cf90838eb4aae49ceffc43a1f19df0504
paroleon <>
2021-04-07 01:08:05 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//Generar numero aaaaaaaleatorio
+const num= Math.random()*10
+console.log(num)
+
+const r= Math.floor(Math.random()*10+1)
+console.log(r)
+//por que no var
+if(true){
+  var color = "negro"
+  let tamano = "alto"
+  //uno espera que las variables no se vean fuera de estas llaves
+}
+console.log(color)
+//console.log(tamano)
